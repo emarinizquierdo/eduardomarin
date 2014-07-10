8ae937636d00f92ae319b25a088fc5390b8cfca2
Eduardo Marin <edu@nevicaneser.(none)>
2014-07-10 21:09:15 +0200

    updated

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('eduardomarinFsApp')
+  .controller('LoginCtrl', function ($scope, Auth, $location) {
+    $scope.user = {};
+    $scope.errors = {};
+
+    $scope.login = function(form) {
+      $scope.submitted = true;
+      
+      if(form.$valid) {
+        Auth.login({
+          email: $scope.user.email,
+          password: $scope.user.password
+        })
+        .then( function( p_user ) {
+          if(p_user.role == 'admin'){
+            $location.path("/admin");
+          }
+        })
+        .catch( function(err) {
+          err = err.data;
+          $scope.errors.other = err.message;
+        });
+      }
+    };
+  });
